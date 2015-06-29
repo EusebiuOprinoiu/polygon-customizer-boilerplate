35514c9a7007452d74893b689420738e261173d0
Eusebiu Oprinoiu <eusebiu.websites@outlook.com>
2015-06-29 03:49:31 +0300

    Active callback example added
    Added an active callback example.

--- custom-callbacks/active-callbacks.php
+++ custom-callbacks/active-callbacks.php
@@ -11,4 +11,26 @@
 
 
 
-// Do something
+if ( ! function_exists( 'polygon_active_callbacks' ) ) {
+
+	/**
+	 * Active callback.
+	 *
+	 * Display options in the customizer only if certain conditions are met.
+	 *
+	 * @since    1.0.0
+	 */
+	function polygon_active_callbacks( $control ) {
+		// Callbacks related to 'temporary_option'
+		$setting_value = $control->manager->get_setting( 'temporary_option' )->value();
+		$control_id    = $control->id;
+
+		if ( $control_id == 'temporary_option_one' && $setting_value == 'first-option' ) return true;
+		if ( $control_id == 'temporary_option_two' && $setting_value == 'second-option' ) return true;
+
+
+
+		// Return false if no conditions are met
+		return false;
+	}
+}
